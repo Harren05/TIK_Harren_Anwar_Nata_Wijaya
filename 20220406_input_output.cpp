b3e547907f39364b1c6ae83b28b21c887971ac3b
Harren05 <103086043+Harren05@users.noreply.github.com>
2022-04-06 11:27:30 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() 
+{//begin of fungction 
+    string nama; 
+    cout<<"Masukkan nama: "; cin >>nama;
+    cout<<"Nama Anda: "<<nama;
+    
+}//end of funnction 
