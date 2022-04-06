e45ad8771972a2ce4acb501586fa4bf1522510b2
Harren05 <103086043+Harren05@users.noreply.github.com>
2022-04-06 11:11:28 +0800

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,25 @@
+//header 
+#include <iostream>
+using namespace std;
+int main() 
+{//begin of fungction 
+//variable nama manifestasi data (Variabel bisa di ubah)
+
+        string nama;// deklarasi variabel > string (Kalimat/karakter yang panjang)
+        nama = "Harren Anwar Nata Wijaya";// masukkkan value kedalam variabel
+        
+        int angka;// angka bilangan bulat
+        angka = 20;
+        
+        float a,b;
+        a= 10.21;
+        b= 3.14;
+        // float > angka bilangan desimal
+        
+        cout<<nama<<endl;// menampilkan variabel
+        cout<<angka<<endl;
+        cout<<a*b<<endl;
+        cout<<a*b + a/b<<endl;
+        //cout // untuk menampilkan apapun
+
+}//end of funnction 
