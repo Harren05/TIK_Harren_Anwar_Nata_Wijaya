a26783e19cf7598d0e4e67bc5d533ab79735c46a
Harren05 <103086043+Harren05@users.noreply.github.com>
2022-04-13 11:31:43 +0800

    Update 20220413Percabangan.cpp

--- 20220413Percabangan.cpp
+++ 20220413Percabangan.cpp
@@ -136,3 +136,37 @@ int main(){
     cout<<keterangan;
 }
 
+
+
+
+
+//nested if 
+//percabagan bersarang
+//if didalam if 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nilai;
+    cout<<"Masukkan Nilai "; cin>>nilai;
+    if (nilai % 2 == 0){
+        cout<<"Bilangan Genap"<<endl;
+        if (nilai <10){
+            cout<<"Bilangan genap kurang dari 10"<<endl;
+        }
+        else {
+        cout<<"Bilangan genap lebih dari 10"<<endl;
+        }
+}
+    else {
+        cout<<"Bilangan ganjil"<<endl;
+        if (nilai <10){
+            cout<<"Bilangan ganjil kurang dari 10"<<endl;
+        }
+        else {
+            cout<<"Bilangan ganjil lebih dari 10"<<endl;  
+        }
+        }
+        return 0;
+}
