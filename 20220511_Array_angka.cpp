d8155abdca5e17a2aca62f88d436a3e88d627be2
Harren05 <103086043+Harren05@users.noreply.github.com>
2022-05-11 10:22:23 +0800

    Create 20220511_Array_angka.cpp

---
+++ 20220511_Array_angka.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int angka [] = {
+        123,
+        123,
+        445,
+        5456,
+        67,
+        6678,
+        867,
+        678,
+        6678,
+        678,
+    };
+        
+    
+   for (int j = 0; j<10 ; j++){
+   cout<<angka[j]<<endl;
+}
+
+    return 0;
+}
