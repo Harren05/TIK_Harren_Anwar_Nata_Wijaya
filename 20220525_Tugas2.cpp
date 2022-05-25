639cf9a26b2148cae055a1865a273b283b214d9b
Harren05 <103086043+Harren05@users.noreply.github.com>
2022-05-25 15:53:20 +0700

    Create 20220525_Tugas2.cpp

---
+++ 20220525_Tugas2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    
+    
+    cout<<"Masukkan Nilai X = ";cin>>x;
+    cout<<"Masukkan Nilai Y = ";cin>>y;
+    
+    cout<<"Nilai X = "<<y<<endl;
+    cout<<"Nilai Y = "<<x<<endl;
+
+    return 0;
+}
